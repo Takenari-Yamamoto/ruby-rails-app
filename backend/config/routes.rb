563b08b923bdc1f6cbf5d71d1bd5f19c554d9c0c
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-10-01 07:48:53 +0900

    init client

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index, :show, :create, :destroy, :update]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # Defines the root path route ("/")d
   # root "articles#index"
 end
